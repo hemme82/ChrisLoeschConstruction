5f6bb1591b53107529020c8f72fd2e1f0a1d606f
dave.hemmesch <dave.hemmesch@infinitecampus.com>
2020-06-22 07:11:31 -0500

    separate js

---
+++ scripts/index.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    // first side nav is id. second is method from materialize
+    $('.sidenav').sidenav();
+    $('.materialboxed').materialbox();
+    $('.parallax').parallax();
+    $('.carousel').carousel();
+    $('.scrollspy').scrollSpy();
+
+
+
+    // $('#logout').hide();
+    // $('#uploadPic').hide();
+
+    $("#buttonAdmin").click(function(){
+        $("#loginForm").toggle(500);
+    });
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+  
+    var items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
+  
+  });
+
+var uploader = document.getElementById('uploader');
+var fileButton = document.getElementById('fileButton');
+
+//listen for file selection
+fileButton.addEventListener('change', function(e) {
+    //get file
+    var file = e.target.files[0];
+    // create a storage ref
+    var storageRef = firebase.storage().ref('images/' + file.name);
+    //upload file
+    var task = storageRef.put(file);
+    //update progress bar
+    task.on('state_changed', 
+
+        function progress(snapshot) {
+            var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            uploader.value = percentage;
+            console.log(percentage);
+        },
+
+        function error(err) {
+
+        },
+
+        function complete() {
+
+        }
+    )
+})
+
