085729e59883b282d8149d3190fc63be4c16f446
dave.hemmesch <dave.hemmesch@infinitecampus.com>
2020-06-29 14:08:36 -0500

    pic upload works

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,64 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.materialboxed').materialbox();
     $('.parallax').parallax();
-    $('.carousel').carousel();
     $('.scrollspy').scrollSpy();
+     //init carousel
+    var slider = $('.carousel');
+    slider.carousel();
+    //  add a new item
+	// slider.append(
+    //     '<a class="carousel-item active" href="#eleven!"><img src="http://lorempixel.com/250/250/nature/3"></a>');
+     
+        var arrayOfImages = [];
+        var storageReference = firebase.storage().ref();
+        var folderRef = storageReference.child("images");
+    
+        folderRef
+            .listAll()
+            .then(result => {
+                // result.prefixes.forEach(folderRef => {
+                //     console.log("folder == ", folderRef);
+                // });
+                result.items.forEach(itemRef => {
+                    itemRef.getDownloadURL().then(url => {
+                        slider.append(`
+                        <a class="carousel-item"><img src=${url}></a>
+                        `);
+                        if (slider.hasClass('initialized')){
+                            slider.removeClass('initialized')
+                            }
+                
+                            //just reinit the carousel
+                            slider.carousel();
+                            
+                            
+                        console.log(url);
+                            arrayOfImages.push(url);
+                        
+                    })
+                    
+                });
+                console.log(arrayOfImages);
+            })
+            // .catch(error => {
+            //     console.log("error == ", error);
+            // });
+    
+        
+        // arrayOfImages.forEach(link => {
+        //     console.log(link);
+        //     slider.append(
+        //         '<a class="carousel-item active" href="#eleven!"><img src="http://lorempixel.com/250/250/nature/3"></a>');
+            
+            // let carousel = document.createElement("div");
+            // carousel.className = "carousel-item";
+            // carousel.innerHTML = '<img src="img/floor_plan.jpg">';
+            // document.body.append(carousel);
 
-
+            //remove the 'initialized' class which prevents slider from initializing itself again when it's not needed
+            
+    
+    
 
     // $('#logout').hide();
     // $('#uploadPic').hide();
@@ -20,9 +74,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var modals = document.querySelectorAll('.modal');
     M.Modal.init(modals);
-  
-    var items = document.querySelectorAll('.collapsible');
-    M.Collapsible.init(items);
   
   });
 
@@ -46,6 +97,13 @@ fileButton.addEventListener('change', function(e) {
             var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
             uploader.value = percentage;
             console.log(percentage);
+            if(percentage == 100){
+                setTimeout(
+                    function() 
+                    {
+                        $('#modal-create').hide();
+                    }, 500);
+            }
         },
 
         function error(err) {
@@ -53,13 +111,17 @@ fileButton.addEventListener('change', function(e) {
         },
 
         function complete() {
-            storageRef.getDownloadURL().then(function(url){
-                var img = document.getElementById('testID');
-                img.src = url;
-                console.log(url);
-            })
+            // storageRef.getDownloadURL().then(function(url){
+            //     var img = document.getElementById('testID');
+            //     img.src = url;
+            //     console.log(url);
+                
+                
+            // })
 
         }
     )
 })
+
+
 
